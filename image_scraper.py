d1ab281a7922010fc3274416471430476ffa1599
Peter Johanns <johannspeter@web.de>
2024-11-23 19:44:26 +0100

    adjusted image_scraper.py

--- image_scraper.py
+++ image_scraper.py
@@ -1,30 +1,39 @@
 from google_images_search import GoogleImagesSearch
 import dotenv
 import os
+from typing import List
+
 # you can provide API key and CX using arguments,
 # or you can set environment variables: GCS_DEVELOPER_KEY, GCS_CX
 dotenv.load_dotenv()
 
 gis = GoogleImagesSearch(os.getenv('GCS_DEVELOPER_KEY'), os.getenv('GCS_CX'))
 
-# define search params
-# option for commonly used search param are shown below for easy reference.
-# For param marked with '##':
-#   - Multiselect is currently not feasible. Choose ONE option only
-#   - This param can also be omitted from _search_params if you do not wish to define any value
-_search_params = {
-    'q': 'amnestiy international',
-    'num': 1,
-}
+
+def search_prompts_and_save_imgs(prompts: List[str], save_location_dir: str):
+    result_paths = []
+    for prompt in prompts:
+        _search_params = {
+            'q': prompt,
+            'num': 1,
+            'imgType': 'photo',
+        }
+        gis.search(search_params=_search_params)
+        for image in gis.results():
+            image.download(save_location_dir)
+            result_paths.append(image.path)
+
+    return result_paths
 
 
-# search first, then download and resize afterwards:
-gis.search(search_params=_search_params)
-for image in gis.results():
-    image.url  # image direct url
-    image.referrer_url  # image referrer url (source)
-
-    image.download('.')  # download image
-    image.resize(500, 500)  # resize downloaded image
-
-    image.path  # downloaded local file path
+if __name__ == '__main__':
+    prompts = [
+        "electric vehicle",
+        "solar energy",
+        "renewable energy",
+        "electric car",
+        "e-bike",
+        "solar panel",
+    ]
+    save_location_dir = "images"
+    search_prompts_and_save_imgs(prompts, save_location_dir)
