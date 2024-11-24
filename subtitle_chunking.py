5309e2e3ea39d727586d3e168e87a1d82d58e547
Tom Boettger <t.boettger@live.de>
2024-11-24 04:15:26 +0100

    Add subtitle file generation and time formatting functions

--- subtitle_chunking.py
+++ subtitle_chunking.py
@@ -1,3 +1,6 @@
+import math
+
+
 def split_transcript(transcript, max_length=60):
     words = transcript.split()
     chunks = []
@@ -14,6 +17,36 @@ def split_transcript(transcript, max_length=60):
         chunks.append(" ".join(current_chunk))
 
     return chunks
+
+
+def format_time(seconds):
+    hours = math.floor(seconds / 3600)
+    seconds %= 3600
+    minutes = math.floor(seconds / 60)
+    seconds %= 60
+    milliseconds = round((seconds - math.floor(seconds)) * 1000)
+    seconds = math.floor(seconds)
+    formatted_time = f"{hours:02d}:{minutes:02d}:{seconds:01d},{milliseconds:03d}"
+
+    return formatted_time
+
+
+def generate_subtitle_file(directory, segments):
+    subtitle_file = "subtitles.srt"
+    text = ""
+    for index, segment in enumerate(segments):
+        segment_start = format_time(segment.start)
+        segment_end = format_time(segment.end)
+        text += f"{str(index+1)} \n"
+        text += f"{segment_start} --> {segment_end} \n"
+        text += f"{segment.text} \n"
+        text += "\n"
+
+    f = open(f"{directory}/{subtitle_file}", "w")
+    f.write(text)
+    f.close()
+
+    return subtitle_file
 
 
 if __name__ == "__main__":
